258a1c452ae150609ff8232085f1b1fdbd8f3b90
Gaetano Zangari <gaetano.zangari98@outlook.it>
2026-02-24 11:34:04 +0100

    Add CORS configuration to enable cross-origin requests

---
+++ src/main/java/pw_be/Security/CorsSecurity.java
@@ -0,0 +1,21 @@
+package pw_be.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsSecurity {
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("http://localhost:5173") // Sostituisci con l'URL del tuo frontend
+                            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                            .allowedHeaders("*")
+                            .allowCredentials(true);
+                }
+            };
+        }
+}
